f7333da7752dd0302b939ee3c0a15ab8d347d9b9
crexodon <36489816+crexodon@users.noreply.github.com>
2018-12-05 17:36:21 +0100

    Add files via upload

--- src/io/Statistics.java
+++ src/io/Statistics.java
@@ -1,4 +1,14 @@
 package io;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+import model.Station;
+import model.TheObject;
+import model.TheObject.TheObservable;
+import model.TheObject.TheObservale;
+import model.TheObject.TheObservebale;
+
 
 /**
  * A class for printing statistics
@@ -6,10 +16,9 @@ package io;
  * @author Jaeger, Schmidt
  * @version 2015-11-18
  */
-public class Statistics {
+public class Statistics implements Observer{
 
 	private static String buffer; 
-	
 	
 	/** appends the given String to the buffer
 	 *
@@ -28,5 +37,31 @@ public class Statistics {
 		
 		System.out.println(message);
 	}
+	
+	public boolean isNewProcessTime(int newProcessTime) {
+		return true;
+	}
+	
+	public boolean newTheNextStation(Station newNextStation) {
+		return true;
+	}
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		TheObservable TheObservable= (TheObservable) arg0;
+		Integer info = (Integer)arg1;
+		int newProcessTime = info.intValue();
+		if(isNewProcessTime(TheObservable.getProcessTime())){
+			System.out.println(TheObservable.getObject()+"has  the new process time"+newProcessTime);
+			TheObservable.deleteObservers();
+		}
+		if(newTheNextStation(TheObservable.getNextStation())) {
+			System.out.println(TheObservable.getObject() +"has a new next station"+TheObservable.getNextStation());
+			TheObservable.deleteObservers();
+		}
+	}
+
+
 	
 }
